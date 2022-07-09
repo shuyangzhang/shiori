74cd5f758a3cb8018cb5536e62bee259193f34e7
zhangshuyang <zhangshuyang@outlook.com>
2022-07-09 15:02:12 +0800

    refactor(main): refactor the structure of project

--- main.go
+++ main.go
@@ -4,17 +4,13 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/lonelyevil/khl"
 	"github.com/lonelyevil/khl/log_adapter/plog"
 	"github.com/phuslu/log"
-)
-
-var (
-	VERSION string = "0.1.0"
+	"github.com/shuyangzhang/shiori/handlers"
 )
 
 func main() {
@@ -23,7 +19,7 @@ func main() {
 		Writer: &log.ConsoleWriter{},
 	}
 	s := khl.New(os.Getenv("TOKEN"), plog.NewLogger(&logger))
-	s.AddHandler(messageHandler)
+	handlers.RegisterHandlers(s)
 	s.Open()
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
@@ -33,32 +29,4 @@ func main() {
 
 	// Cleanly close down the KHL session.
 	s.Close()
-}
-
-func messageHandler(ctx *khl.KmarkdownMessageContext) {
-	if ctx.Common.Type != khl.MessageTypeKMarkdown || ctx.Extra.Author.Bot {
-		return
-	}
-
-	if strings.HasPrefix(ctx.Common.Content, ",ping") {
-		ctx.Session.MessageCreate(&khl.MessageCreate{
-			MessageCreateBase: khl.MessageCreateBase{
-				TargetID: ctx.Common.TargetID,
-				Content:  "エンチャントアロー!",
-				Quote:    ctx.Common.MsgID,
-				Type:     khl.MessageTypeKMarkdown,
-			},
-		})
-	}
-
-	if strings.HasPrefix(ctx.Common.Content, ",version") {
-		ctx.Session.MessageCreate(&khl.MessageCreate{
-			MessageCreateBase: khl.MessageCreateBase{
-				TargetID: ctx.Common.TargetID,
-				Content:  fmt.Sprintf("Version: %s", VERSION),
-				Quote:    ctx.Common.MsgID,
-				Type:     khl.MessageTypeKMarkdown,
-			},
-		})
-	}
 }
