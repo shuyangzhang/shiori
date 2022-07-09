be1e97c55a3e4e0057cd6ff5fcdfec444f698505
zhangshuyang <zhangshuyang@outlook.com>
2022-07-10 00:25:13 +0800

    feat(command): parse the command and params

---
+++ tools/command.go
@@ -0,0 +1,24 @@
+package tools
+
+import (
+	"strings"
+
+	"github.com/shuyangzhang/shiori/configs"
+	"golang.org/x/exp/slices"
+)
+
+func GetCommandWithParameters(rawCommand string) (withPrefix bool, command string, params []string) {
+	runeRawCommand := []rune(rawCommand)
+	prefix := string(runeRawCommand[0])
+	commandWithParameters := string(runeRawCommand[1:])
+
+	if slices.Contains(configs.EnvConfigs.AllPrefixes, prefix) {
+		commandSlice := strings.Fields(commandWithParameters)
+
+		withPrefix = true
+		command = commandSlice[0]
+		params = commandSlice[1:]
+	}
+
+	return
+}
